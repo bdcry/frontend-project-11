ed06122b87265e42c0a38dcdca9729bda1d3a07f
Tim <tim@MacBook-Pro-2.local>
2024-07-09 02:09:53 +0300

    step 3 | refactoring, upd view, index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 import * as yup from 'yup';
 import keyBy from 'lodash/keyBy.js';
 import i18next from 'i18next';
-import initView from './view';
+import render from './view';
 import resources from './locales/index';
 
 const i18nextInstance = i18next.createInstance();
@@ -21,7 +21,7 @@ const createSchema = (feeds) => yup
 // notOneOf - принимает на вход массив запрещенных значений (фидов)
 
 const state = {
-  currentLanguage: 'en',
+  currentLanguage: 'ru',
   url: '',
   errors: [],
   feeds: [], // добавленные ссылки для проверки дубликатов
@@ -30,8 +30,9 @@ const state = {
 const input = document.getElementById('url-input');
 const form = document.querySelector('.rss-form');
 const feedback = document.querySelector('.feedback');
+const elements = { input, form, feedback };
 
-const watchedState = initView(state);
+const watchedState = render(state, elements, i18nextInstance);
 
 const handleInputChange = (e) => {
   watchedState.url = e.target.value;
@@ -77,26 +78,12 @@ const handleSubmit = (e) => {
 input.addEventListener('input', handleInputChange);
 form.addEventListener('submit', handleSubmit);
 
-const initLocales = () => {
-  document.querySelector('title').textContent = i18nextInstance.t('title');
-  document.querySelector('h1').textContent = i18nextInstance.t('header');
-  document.querySelector('.lead').textContent = i18nextInstance.t('lead');
-  document.querySelector('label[for="url-input"]').textContent = i18nextInstance.t('url_input');
-  document.querySelector('button[type="submit"').textContent = i18nextInstance.t('button');
-  document.querySelector('.mt-2').textContent = i18nextInstance.t('example');
-  document.querySelector('.footer .text-center').childNodes[0].textContent = i18nextInstance.t('created_by');
-};
-
-initLocales();
-
 document.querySelector('label[data-lng="ru"]').addEventListener('click', () => {
   i18nextInstance.changeLanguage('ru');
   watchedState.currentLanguage = 'ru';
-  initLocales();
 });
 
 document.querySelector('label[data-lng="en"]').addEventListener('click', () => {
   i18nextInstance.changeLanguage('en');
   watchedState.currentLanguage = 'en';
-  initLocales();
 });
--- src/view.js
+++ src/view.js
@@ -1,21 +1,65 @@
 import onChange from 'on-change';
 
-const initView = (state) => {
-  const input = document.getElementById('url-input');
-  const feedback = document.querySelector('.feedback');
+const renderForm = (state, elements) => {
+  const { input, feedback } = elements;
 
-  const watchedState = onChange(state, (path, value) => {
-    if (path === 'errors' && value.length > 0) {
-      input.classList.add('is-invalid');
-      feedback.classList.remove('text-success');
-      feedback.classList.add('text-danger');
-      feedback.textContent = value;
-    } else {
-      input.classList.remove('is-invalid');
-      feedback.textContent = '';
+  if (state.errors.length > 0) {
+    input.classList.add('is-invalid');
+    feedback.classList.remove('text-success');
+    feedback.classList.add('text-danger');
+    feedback.textContent = state.errors;
+  } else {
+    input.classList.remove('is-invalid');
+    feedback.textContent = '';
+  }
+};
+
+const renderLocales = (i18nextInstance) => {
+  document.querySelector('title').textContent = i18nextInstance.t('title');
+  document.querySelector('h1').textContent = i18nextInstance.t('header');
+  document.querySelector('.lead').textContent = i18nextInstance.t('lead');
+  document.querySelector('label[for="url-input"]').textContent = i18nextInstance.t('url_input');
+  document.querySelector('button[type="submit"').textContent = i18nextInstance.t('button');
+  document.querySelector('.mt-2').textContent = i18nextInstance.t('example');
+  document.querySelector('.footer .text-center').childNodes[0].textContent = i18nextInstance.t('created_by');
+};
+
+const renderLanguageButtons = (currentLanguage) => {
+  const ruButton = document.querySelector('label[data-lng="ru"]');
+  const enButton = document.querySelector('label[data-lng="en"]');
+
+  switch (currentLanguage) {
+    case 'ru':
+      ruButton.classList.add('active');
+      enButton.classList.remove('active');
+      break;
+    case 'en':
+      enButton.classList.add('active');
+      ruButton.classList.remove('active');
+      break;
+    default:
+      ruButton.classList.add('active');
+      enButton.classList.remove('active');
+  }
+};
+
+const render = (state, elements, i18nextInstance) => {
+  const watchedState = onChange(state, (path) => {
+    switch (path) {
+      case 'errors':
+        renderForm(state, elements);
+        break;
+      case 'currentLanguage':
+        renderLocales(i18nextInstance);
+        renderLanguageButtons(state.currentLanguage);
+        break;
+      default:
+        break;
     }
   });
+  renderLocales(i18nextInstance);
+  renderLanguageButtons(state.currentLanguage);
   return watchedState;
 };
 
-export default initView;
+export default render;
