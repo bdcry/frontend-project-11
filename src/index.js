0fe9b19603aaad0c19794d2a338572342104dd60
Tim <tim@MacBook-Pro-2.local>
2024-08-25 19:27:52 +0300

    review 1 | remove debugging logs from app

--- src/index.js
+++ src/index.js
@@ -31,8 +31,7 @@ const fetchRSS = (url) => {
   return axios
     .get(proxiedUrl)
     .then((response) => response.data.contents)
-    .catch((error) => {
-      console.log('Error fetching RSS data:', error);
+    .catch(() => {
       throw new Error(i18nextInstance.t('errors.networkError'));
     });
 };
@@ -90,18 +89,14 @@ const checkForUpdates = () => {
           const { posts: newPosts } = parsedData;
           const actualTitles = posts.map((post) => post.title);
           const filteredPosts = newPosts.filter((newPost) => !actualTitles.includes(newPost.title));
-          // console.log(actualTitles);
-          console.log('Выводим новые посты:', newPosts);
-          console.log('Выводим отфильтрованные посты:', filteredPosts);
 
           if (filteredPosts.length > 0) {
             view.posts.push(...filteredPosts);
-            // console.log('Выводим состояние с новыми постами:', view.posts);
           }
         }
       })
       .catch((err) => {
-        console.log('Error fetching feed updates:', err);
+        view.errors = err.response;
       });
   };
   feeds.forEach((feed) => {
@@ -137,7 +132,6 @@ const handleSubmit = (e) => {
             generateId,
           );
           if (parsedData) {
-            console.log('Parsed RSS data:', parsedData);
             const { title, description, posts } = parsedData;
             // Если все ок, то пушим в фиды и в посты наши данные
             const feed = { title, description, link: url };
@@ -145,9 +139,6 @@ const handleSubmit = (e) => {
             view.posts.push(...posts.flatMap((postArray) => postArray));
             // разбиваем данные на отдельные части, тк без спред оператора
             // приходит вложенный массив, который мы не можем посмотреть
-            // console.log('Feeds', state.feeds);
-            // console.log('Posts', state.posts);
-            // console.log('Состояние', state);
             view.url = '';
             input.value = '';
             input.focus();
